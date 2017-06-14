ce10deb28c647a02c9ef6005c824cc93c13aa056
unionset <abc@example.com>
2017-06-14 20:03:03 +0200

    added plot.py, updated train.py and test.py

--- test.py
+++ test.py
@@ -19,11 +19,7 @@ test_list_path  = sample_dir_path+"test.list"
 labels_path = sample_dir_path+"labels.txt"
 
 network_dir_path = "/network/"
-
-layer_1_save_path = network_dir_path+"layer_1"
-layer_2_save_path = network_dir_path+"layer_2"
-layer_3_save_path = network_dir_path+"layer_3"
-layer_4_save_path = network_dir_path+"layer_4"
+test_error_path = network_dir_path+"testing_error"
 
 exit_flag = False
 start_time = time.time()
@@ -34,63 +30,70 @@ labels_list = open(labels_path).readlines()
 ###################################################################################################
 print "layer 1 initialization..."
 input_depth = 3
-num_of_filter = 64
-field_size = 3
-stride = 1
-zero_pad = 1
+num_of_filter = 8
+field_size = 4
+stride = field_size
+zero_pad = 0
 layer_1 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
-if (os.path.exists(layer_1_save_path)):
-    print "Loading from ",layer_1_save_path
-    layer_1.load_layer(layer_1_save_path)
-else:
-    print "Unable to open layer 1 from",layer_1_save_path
 print "################################################################################"
 ###################################################################################################
+print "layer 2 initialization..."
 input_depth = num_of_filter
-field_size = 2
-stride = 2
-layer_2 = layer.max_pooling_layer(input_depth, field_size, stride)
+num_of_filter = 16
+field_size = 4
+stride = field_size
+zero_pad = 0
+layer_2 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
+print "################################################################################"
 ###################################################################################################
 print "layer 3 initialization..."
-num_of_input = 16384
-num_of_output = 10
-layer_3 = layer.fully_connected_layer(num_of_input,num_of_output,empty,empty)
-if (os.path.exists(layer_3_save_path)):
-    print "Loading from ",layer_3_save_path
-    layer_3.load_layer(layer_3_save_path)
-else:
-    print "Unable to open layer 3 from",layer_3_save_path
+input_depth = num_of_filter
+num_of_filter = 10
+field_size = 2
+stride = field_size
+zero_pad = 0
+layer_3 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
 print "################################################################################"
 ###################################################################################################
 layer_4 = layer.softmax_layer()
-###################################################################################################
-# Trial
-logo = SimpleCV.Image(test_list[0][:-1])
-input_image = logo.getNumpy()
-input_image = change_image_format(input_image)
-numpy.seterr(all='warn')
-print "validating layer_1..."
-layer_1.check_input_validity(input_image)
-output_1 = layer_1.propagate_forward(input_image)
-print "validating layer_2..."
-layer_2.check_input_validity(output_1)
-output_2 = layer_2.propagate_forward(output_1)
-print "validating layer_3..."
-layer_3.check_input_validity(output_2)
-output_3 = layer_3.propagate_forward(output_2)
-print "validating layer_4..."
-output_4 = layer_4.propagate_forward(output_3)
 ###################################################################################################
 labels_stat = numpy.zeros(len(labels_list))
 total_error = 0
 total_match = 0
 accuracy = 0
-start = 0
-end = 500
+if (os.path.exists(test_error_path)):
+    test_error = open(test_error_path).readlines()
+    epoch_num = len(test_error)+1
+else:
+    epoch_num = 1
+
+print
+print "Waiting epoch",epoch_num,"to complete..."
 while (True):
-    print
-    print "Start testing from sample:",start
-    for sample_cnt in range(start, end):
+    layer_1_save_path = network_dir_path+"epoch_"+str(epoch_num)+"_layer_1_completed"
+    layer_2_save_path = network_dir_path+"epoch_"+str(epoch_num)+"_layer_2_completed"
+    layer_3_save_path = network_dir_path+"epoch_"+str(epoch_num)+"_layer_3_completed"
+    if (os.path.exists(layer_1_save_path)):
+        print "Loading from ",layer_1_save_path
+        layer_1.load_layer(layer_1_save_path)
+    else:
+        time.sleep(10)
+        continue
+    if (os.path.exists(layer_2_save_path)):
+        print "Loading from ",layer_2_save_path
+        layer_2.load_layer(layer_2_save_path)
+    else:
+        time.sleep(10)
+        continue
+    if (os.path.exists(layer_3_save_path)):
+        print "Loading from ",layer_3_save_path
+        layer_3.load_layer(layer_3_save_path)
+    else:
+        time.sleep(10)
+        continue
+    print "********************************************************************************"
+    print "Testing after epoch",epoch_num
+    for sample_cnt in range(0, len(test_list)):
         test_sample_label = re.split('[,_,.]', test_list[sample_cnt])[1]
         cost = numpy.zeros(len(labels_list))
         for cnt in range(0, len(labels_list)):
@@ -105,41 +108,39 @@ while (True):
         logo = SimpleCV.Image(test_list[sample_cnt][:-1])
         input_image = logo.getNumpy()
         input_image = change_image_format(input_image)
-
         output_1 = layer_1.propagate_forward(input_image)
         output_2 = layer_2.propagate_forward(output_1)
         output_3 = layer_3.propagate_forward(output_2)
         output_4 = layer_4.propagate_forward(output_3)
-        error_0 = output_4 - cost
-
+        error_0 = output_4-cost
         error_pos = numpy.argmax(output_4)
         transformed_error_0 = numpy.zeros(output_4.shape)
         transformed_error_0[error_pos] = 1
+        total_error += numpy.sum(error_0*error_0)
         if (numpy.sum(transformed_error_0*cost) == 1):
             total_match += 1.
             labels_stat += cost
-        total_error += numpy.sum(error_0*error_0)
-        if ((sample_cnt+1)%5 == 0):
-            time_passed = time.time()-start_time
-            hours = int(time_passed)/3600
-            minutes = int(time_passed-3600*hours)/60
-            print
-            print "Computation duration:",hours,"hrs,",minutes,"min,",time_passed%60,"sec"
-            print "Sample counter:",sample_cnt
-            print "Label:",test_sample_label
-            print "Current accuracy:",total_match/(sample_cnt-start+1)
-            print "Current total error:",0.5*total_error/(sample_cnt-start+1)
-            print "Error:"
-            print error_0
-    break
-print
-print "********************************************************************************"
-time_passed = time.time()-start_time
-hours = int(time_passed)/3600
-minutes = int(time_passed-3600*hours)/60
-print "Computation duration:",hours,"hrs,",minutes,"min,",time_passed%60,"sec"
-print "Total sample number:",end-start
-print "Total accuracy:",total_match/(end-start)
-print "Total error:",total_error
-print "Label statistics:",labels_stat
-print "Label list:",labels_list[:][:-1]
+    temp_file = open(test_error_path, 'a+')
+    temp_file.write("%s\n" % str(total_error/len(test_list)))
+    temp_file.close()
+    print "********************************************************************************"
+    print "================================================================================"
+    time_passed = time.time()-start_time
+    hours = int(time_passed)/3600
+    minutes = int(time_passed-3600*hours)/60
+    print "Computation duration:",hours,"hrs,",minutes,"min,",time_passed%60,"sec"
+    print "Total sample number:",len(test_list)
+    print "Total error:",total_error
+    print "Total accuracy:",total_match/len(test_list)
+    print "Label statistics:",labels_stat
+    print "Label list:",labels_list[:][:-1]
+    print "Average testing error:",total_error/len(test_list)
+    print "================================================================================"
+    print "********************************************************************************"
+    total_match = 0
+    total_error = 0
+    labels_stat = 0
+    epoch_num = epoch_num+1
+    print
+    print "Waiting epoch",epoch_num,"to complete..."
+###################################################################################################
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import re
 # Core utilities
 import SimpleCV
 import random
+import pickle
 import numpy
 import layer
 
@@ -66,18 +67,25 @@ sample_dir_path = "/cifar/"
 labels_path = sample_dir_path+"labels.txt"
 
 network_dir_path = "/network/"
+progress_path = network_dir_path+"progress_of_training"
 train_list_path = network_dir_path+"train.list"
-progress_path = network_dir_path+"progess_of_training"
+#train_list_path = network_dir_path+"valid.list"
 epoch_path = network_dir_path+"current_epoch"
+
+train_error_path = network_dir_path+"training_error"
 
 layer_1_save_path = network_dir_path+"layer_1"
 layer_2_save_path = network_dir_path+"layer_2"
 layer_3_save_path = network_dir_path+"layer_3"
-layer_4_save_path = network_dir_path+"layer_4"
+
+optimizer_dir_path = network_dir_path+"optimizer/"
+optimizer_1_save_path = optimizer_dir_path+"layer_1_optimizer"
+optimizer_2_save_path = optimizer_dir_path+"layer_2_optimizer"
+optimizer_3_save_path = optimizer_dir_path+"layer_3_optimizer"
 
 exit_flag = False
 is_training = False
-epoch_cnt = 0
+epoch_cnt = 1
 last_sample_cnt = 0
 start_time = time.time()
 empty = numpy.array([])
@@ -95,37 +103,70 @@ else:
 if os.path.isfile(progress_path):
     last_sample_cnt = int(open(progress_path).readline())
     is_training = True
+
+numpy_settings = numpy.seterr(all='raise')
 ###################################################################################################
 print "layer 1 initialization..."
 input_depth = 3
-num_of_filter = 64
-field_size = 3
-stride = 1
-zero_pad = 1
+num_of_filter = 8
+field_size = 4
+stride = field_size
+zero_pad = 0
 layer_1 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
+optimizer_1 = Adam_optimizer()
 if (is_training):
     if (os.path.exists(layer_1_save_path)):
         print "Loading from ",layer_1_save_path
         layer_1.load_layer(layer_1_save_path)
     else:
         print "Unable to open layer 1 from",layer_1_save_path
+    if (os.path.exists(optimizer_1_save_path)):
+        print "Loading from ",optimizer_1_save_path
+        optimizer_1.load_optimizer(optimizer_1_save_path)
+    else:
+        print "Unable to open optimizer 1 from",optimizer_1_save_path
 print "################################################################################"
 ###################################################################################################
+print "layer 2 initialization..."
 input_depth = num_of_filter
-field_size = 2
-stride = 2
-layer_2 = layer.max_pooling_layer(input_depth, field_size, stride)
+num_of_filter = 16
+field_size = 4
+stride = field_size
+zero_pad = 0
+layer_2 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
+optimizer_2 = Adam_optimizer()
+if (is_training):
+    if (os.path.exists(layer_2_save_path)):
+        print "Loading from ",layer_2_save_path
+        layer_2.load_layer(layer_2_save_path)
+    else:
+        print "Unable to open layer 2 from",layer_2_save_path
+    if (os.path.exists(optimizer_2_save_path)):
+        print "Loading from ",optimizer_2_save_path
+        optimizer_2.load_optimizer(optimizer_2_save_path)
+    else:
+        print "Unable to open optimizer 2 from",optimizer_2_save_path
+print "################################################################################"
 ###################################################################################################
 print "layer 3 initialization..."
-num_of_input = 16384
-num_of_output = 10
-layer_3 = layer.fully_connected_layer(num_of_input,num_of_output,empty,empty)
+input_depth = num_of_filter
+num_of_filter = 10
+field_size = 2
+stride = field_size
+zero_pad = 0
+layer_3 = layer.convolutional_layer(input_depth,num_of_filter,field_size,stride,zero_pad,empty,empty)
+optimizer_3 = Adam_optimizer()
 if (is_training):
     if (os.path.exists(layer_3_save_path)):
         print "Loading from ",layer_3_save_path
         layer_3.load_layer(layer_3_save_path)
     else:
         print "Unable to open layer 3 from",layer_3_save_path
+    if (os.path.exists(optimizer_3_save_path)):
+        print "Loading from ",optimizer_3_save_path
+        optimizer_3.load_optimizer(optimizer_3_save_path)
+    else:
+        print "Unable to open optimizer 3 from",optimizer_3_save_path
 print "################################################################################"
 ###################################################################################################
 layer_4 = layer.softmax_layer()
@@ -138,37 +179,41 @@ numpy.seterr(all='warn')
 print "validating layer_1..."
 layer_1.check_input_validity(input_image)
 output_1 = layer_1.propagate_forward(input_image)
+print "layer_1 shape:",layer_1.weights.shape
+print "output_1 size:",output_1.shape
 print "validating layer_2..."
 layer_2.check_input_validity(output_1)
 output_2 = layer_2.propagate_forward(output_1)
+print "layer_2 shape:",layer_2.weights.shape
+print "output_2 size:",output_2.shape
 print "validating layer_3..."
 layer_3.check_input_validity(output_2)
 output_3 = layer_3.propagate_forward(output_2)
-print "validating layer_4..."
+print "layer_3 shape:",layer_3.weights.shape
+print "output_3 size:",output_3.shape
 output_4 = layer_4.propagate_forward(output_3)
 ###################################################################################################
+#batch_size = len(train_list)/100
 batch_size = 500
-friction_coefficient = 0.5
-regularization_magnitude = 0.01
-relative_update_magnitude = 0.001
+#regularization_magnitude = 1e-5
+#dropout = 0.5 # probability of keeping a unit active. higher = less dropout
+#decay = 1.0
+total_error = 0
+min_average_error = 1.0
 while (True):
     Gw3 = numpy.zeros(layer_3.weights.shape)
     Gb3 = numpy.zeros(layer_3.bias.shape)
+    Gw2 = numpy.zeros(layer_2.weights.shape)
+    Gb2 = numpy.zeros(layer_2.bias.shape)
     Gw1 = numpy.zeros(layer_1.weights.shape)
     Gb1 = numpy.zeros(layer_1.bias.shape)
-    V_Gw3 = numpy.zeros(layer_3.weights.shape)
-    V_Gb3 = numpy.zeros(layer_3.bias.shape)
-    V_Gw1 = numpy.zeros(layer_1.weights.shape)
-    V_Gb1 = numpy.zeros(layer_1.bias.shape)
-    if (epoch_cnt > 0):
-        friction_coefficient = friction_coefficient*(1.0-(numpy.power(0.5, epoch_cnt+1)))/0.5
-        regularization_magnitude *= numpy.power(0.5, epoch_cnt)
-        relative_update_magnitude *= numpy.power(0.5, epoch_cnt)
+    #if (epoch_cnt > 1):
+        #regularization_magnitude *= numpy.power(decay, epoch_cnt)
+        #dropout = (dropout/(1.0-decay))*(1.0-numpy.power(decay, epoch_cnt+1))
     print
     print "Current epoch:",epoch_cnt
-    print "Learning momentum:",friction_coefficient
-    print "Learning magnitude:",relative_update_magnitude
-    print "Regularization magnitude:",regularization_magnitude
+    #print "Regularization magnitude:",regularization_magnitude
+    #print "Dropout:",dropout
     print "Start training from sample:",last_sample_cnt
     for sample_cnt in range(last_sample_cnt, len(train_list)):
         train_sample_label = re.split('[,_,.]', train_list[sample_cnt])[1]
@@ -185,102 +230,121 @@ while (True):
         logo = SimpleCV.Image(train_list[sample_cnt][:-1])
         input_image = logo.getNumpy()
         input_image = change_image_format(input_image)
-
         output_1 = layer_1.propagate_forward(input_image)
+        #Md1 = (numpy.random.rand(*output_1.shape)<dropout)/dropout
+        #output_1 *= Md1
         output_2 = layer_2.propagate_forward(output_1)
+        #Md2 = (numpy.random.rand(*output_2.shape)<dropout)/dropout
+        #output_2 *= Md2
         output_3 = layer_3.propagate_forward(output_2)
         output_4 = layer_4.propagate_forward(output_3)
-        error_0 = output_4 - cost
+        error_0 = output_4-cost
         error_1 = layer_4.propagate_backward(error_0)
-        (error_2, gw3, gb3) = layer_3.propagate_backward(error_1, output_2, True)
-        error_3 = layer_2.propagate_backward(error_2)
-        (error_4, gw1, gb1) = layer_1.propagate_backward(error_3, True)
+        (error_2, gw3, gb3) = layer_3.propagate_backward(error_1)
+        #error_2 *= Md2
+        (error_3, gw2, gb2) = layer_2.propagate_backward(error_2)
+        #error_3 *= Md1
+        (error_4, gw1, gb1) = layer_1.propagate_backward(error_3)
         Gw3 += gw3
         Gb3 += gb3
+        Gw2 += gw2
+        Gb2 += gb2
         Gw1 += gw1
         Gb1 += gb1
-
-        if (sample_cnt%5 == 0):
+        total_error += numpy.sum(error_0*error_0)
+        #if ((sample_cnt+1)%(batch_size*5) == 0):
+            #time_passed = time.time()-start_time
+            #hours = int(time_passed)/3600
+            #minutes = int(time_passed-3600*hours)/60
+            #print
+            #print "Computation duration:",hours,"hrs,",minutes,"min,",time_passed%60,"sec"
+            #print "Counter:",sample_cnt
+            #print "Label:",train_sample_label
+            #print "Error sum:",0.5*numpy.sum(numpy.square(error_0))
+            #print "Error:"
+            #print error_0
+        if ((sample_cnt+1)%batch_size == 0):
+            # regularize weights
+            #layer_3.regularize_weights(0, regularization_magnitude)
+            #layer_2.regularize_weights(0, regularization_magnitude)
+            #layer_1.regularize_weights(0, regularization_magnitude)
+            # update delta weights
+            (V_Gw3, V_Gb3) = optimizer_3.calculate_delta(Gw3, Gb3)
+            (V_Gw2, V_Gb2) = optimizer_2.calculate_delta(Gw2, Gb2)
+            (V_Gw1, V_Gb1) = optimizer_1.calculate_delta(Gw1, Gb1)
+            # update the weights
+            #V_Gw2[:,:-1,:,:] = 0
+            #V_Gw1[:-1,:,:,:] = 0
+            #V_Gb1[:-1] = 0
+            layer_3.update_weights(V_Gw3, V_Gb3)
+            layer_2.update_weights(V_Gw2, V_Gb2)
+            layer_1.update_weights(V_Gw1, V_Gb1)
+            Gw3 = numpy.zeros(layer_3.weights.shape)
+            Gb3 = numpy.zeros(layer_3.bias.shape)
+            Gw2 = numpy.zeros(layer_2.weights.shape)
+            Gb2 = numpy.zeros(layer_2.bias.shape)
+            Gw1 = numpy.zeros(layer_1.weights.shape)
+            Gb1 = numpy.zeros(layer_1.bias.shape)
+            layer_1.save_layer(layer_1_save_path)
+            layer_2.save_layer(layer_2_save_path)
+            layer_3.save_layer(layer_3_save_path)
+            optimizer_1.save_optimizer(optimizer_1_save_path)
+            optimizer_2.save_optimizer(optimizer_2_save_path)
+            optimizer_3.save_optimizer(optimizer_3_save_path)
+            temp_file = open(progress_path, 'w')
+            temp_file.write(str(sample_cnt+1))
+            temp_file.close
+            #print
+            #print "##############################"
+            #print "Saving completed..."
+            #print "##############################"
+        if ((sample_cnt+1)%(batch_size*5) == 0):
             time_passed = time.time()-start_time
             hours = int(time_passed)/3600
             minutes = int(time_passed-3600*hours)/60
             print
             print "Computation duration:",hours,"hrs,",minutes,"min,",time_passed%60,"sec"
-            print "Counter:",sample_cnt
-            print "Label:",train_sample_label
-            print "Error sum:",0.5*numpy.sum(numpy.square(error_0))
-            print "Error:"
-            print error_0
-
-        if ((sample_cnt+1)%batch_size == 0):
-            # regularize weights
-            layer_3.regularize_weights(regularization_magnitude)
-            layer_1.regularize_weights(regularization_magnitude)
-            # evaluate learning rate
-            w3_scale = numpy.linalg.norm(layer_3.weights.ravel())
-            w1_scale = numpy.linalg.norm(layer_1.weights.ravel())
-            Gw3_scale = numpy.linalg.norm(Gw3.ravel())
-            Gw1_scale = numpy.linalg.norm(Gw1.ravel())
-            learning_rate_3 = (relative_update_magnitude*w3_scale)/Gw3_scale
-            learning_rate_1 = (relative_update_magnitude*w1_scale)/Gw1_scale
-            # update training velocity
-            V_Gw3 = friction_coefficient*V_Gw3 - learning_rate_3*Gw3
-            V_Gb3 = friction_coefficient*V_Gb3 - learning_rate_3*Gb3
-            V_Gw1 = friction_coefficient*V_Gw1 - learning_rate_1*Gw1
-            V_Gb1 = friction_coefficient*V_Gb1 - learning_rate_1*Gb1
-            # update the weights
-            layer_3.update_weights(V_Gw3, V_Gb3, -1.0)
-            layer_1.update_weights(V_Gw1, V_Gb1, -1.0)
-            Gw3 = numpy.zeros(layer_3.weights.shape)
-            Gb3 = numpy.zeros(layer_3.bias.shape)
-            Gw1 = numpy.zeros(layer_1.weights.shape)
-            Gb1 = numpy.zeros(layer_1.bias.shape)
-            layer_1.save_layer(layer_1_save_path)
-            layer_3.save_layer(layer_3_save_path)
-            temp_file = open(progress_path, 'w')
-            temp_file.write(str(sample_cnt+1))
-            temp_file.close
-            print
+            print "Epoch",epoch_cnt,"progress:",100.0*(sample_cnt+1.0)/len(train_list),"%"
             print "layer_1 velocity:",numpy.linalg.norm(V_Gw1.ravel())
+            print "layer_2 velocity:",numpy.linalg.norm(V_Gw2.ravel())
             print "layer_3 velocity:",numpy.linalg.norm(V_Gw3.ravel())
-            print "layer_1 learning rate:",learning_rate_1
-            print "layer_3 learning rate:",learning_rate_3
-            print "##############################"
-            print "Saving completed..."
-            print "##############################"
-
-        if ((sample_cnt+1)%5000 == 0):
-            checkpoint = (sample_cnt+1)/5000
-            layer_1.save_layer(layer_1_save_path+str(sample_cnt+1))
-            layer_3.save_layer(layer_3_save_path+str(sample_cnt+1))
-            layer_1.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"layer_1_"+str(checkpoint))
-            layer_3.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"layer_3_"+str(checkpoint))
-            print
-            print "##############################"
-            print "Checkpoint saving completed..."
-            print "##############################"
-        
     # finish current epoch
-    layer_1.save_layer(layer_1_save_path)
-    layer_3.save_layer(layer_3_save_path)
-    layer_1.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"layer_1")
-    layer_3.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"layer_3")
+    #optimizer_1.reset()
+    #optimizer_2.reset()
+    #optimizer_3.reset()
+    optimizer_1.save_optimizer(optimizer_1_save_path)
+    optimizer_2.save_optimizer(optimizer_2_save_path)
+    optimizer_3.save_optimizer(optimizer_3_save_path)
+    layer_1.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"_layer_1_completed")
+    layer_2.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"_layer_2_completed")
+    layer_3.save_layer(network_dir_path+"epoch_"+str(epoch_cnt)+"_layer_3_completed")
+    if (total_error/len(train_list) <  min_average_error):
+        layer_1.save_layer(network_dir_path+"min_layer_1_completed")
+        layer_2.save_layer(network_dir_path+"min_layer_2_completed")
+        layer_3.save_layer(network_dir_path+"min_layer_3_completed")
+        min_average_error = total_error/len(train_list)
+    # record training error
+    temp_file = open(train_error_path, 'a+')
+    temp_file.write("%s\n" % str(total_error/len(train_list)))
+    temp_file.close()
+    last_sample_cnt = 0
+    temp_file = open(progress_path, 'w')
+    temp_file.write(str(last_sample_cnt))
+    temp_file.close
+    print
+    print "Average training error:",total_error/len(train_list)
+    print "##############################"
+    print "Epoch",epoch_cnt,"completed..."
+    print "##############################"
     # shuffle the train dataset
     random.shuffle(train_list)
     temp_file = open(train_list_path, 'w')
     for item in train_list:
         temp_file.writelines("%s" % item)
-    temp_file.close
-    last_sample_cnt = 0
-    temp_file = open(progress_path, 'w')
-    temp_file.write(str(last_sample_cnt))
-    temp_file.close
+    temp_file.close()
     epoch_cnt += 1
     temp_file = open(epoch_path, 'w')
     temp_file.write(str(epoch_cnt))
     temp_file.close
-    print
-    print "##############################"
-    print "Saving completed..."
-    print "##############################"
+    total_error = 0
 ###################################################################################################
